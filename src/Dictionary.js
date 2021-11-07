73a89ecbcaeb37e992e33d567346cd50170890f7
betty2002o <betty2002o@gmail.com>
2021-11-06 21:16:26 -0700

    api added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+
+  function fetchData(response) {
+    console.log(response.data[0]);
+  }
+
   function onSubmit(event) {
     event.preventDefault();
-    alert(keyword);
+    const API = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(API).then(fetchData);
   }
 
   function handleChangeKeyword(event) {
@@ -17,7 +24,7 @@ export default function Dictionary() {
         <input
           type="search"
           autoFocus={true}
-          placeholder="Enter A Word..."
+          placeholder="Search For A Word..."
           onChange={handleChangeKeyword}
         ></input>
         <input type="submit" className="submit-button"></input>
